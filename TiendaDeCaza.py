b4102b12fe4da053264f09f2a3cfeb75278f54b3
JOTAROchl <167789565+JOTAROchl@users.noreply.github.com>
2024-05-30 11:59:07 -0400

    Update and rename TiendaDeCaza to TiendaDeCaza.py

---
+++ TiendaDeCaza.py
@@ -0,0 +1,17 @@
+Usuario=[]
+Contraseña=[]
+ContraseñaRepetida=[]
+Inventario=[]
+OpUsu=0
+print("Hola bienvenidos a 'Tienda de caza generica'.")
+print("inicie sesión para entrar (si no es asi registrese)")
+print()
+print("""--------CACERÍA GENERICA--------
+1.-Registrarse.
+2.-Iniciar sesión.
+--------------------------------""")
+if OpUsu==1:
+  print("Usted ha seleccionado registrarse en el programa de la 'tienda de cacería generica'.")
+  print()
+  print("Nombre de usuario(Solo de puede):")
+  
