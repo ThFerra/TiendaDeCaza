5072011ba930bb8eb63af3500ec029b702179682
ThFerra <167789575+ThFerra@users.noreply.github.com>
2024-06-06 11:38:10 -0400

    TDC.1.13.py
    Update of 1.12

--- TDC.1.1.py
+++ TDC.1.1.py
@@ -1,19 +1,9 @@
 Usuarios=["ju216749340","th212147895"]
-Contraseñas=["contragen1",""]#aqui pon tu contraseña
+Contraseñas=["contragen1","papasfritask"]#aqui pon tu contraseña
 ContraseñaCon=[]
 Inventario=[]
 OpUser=0
 Nom=0
-Con=0
-ConCon=0
-print("Hola bienvenidos a 'Tienda de caza generica'.")
-print("inicie sesión para entrar (si no es asi registrese)")
-print()
-print("""--------CACERÍA GENERICA--------
-1.-Registrarse.
-2.-Iniciar sesión.
---------------------------------""")
-OpUser=int(input(">> "))
 if OpUser==1:
     print("Usted ha seleccionado registrarse en el programa de la 'tienda de cacería generica'.")
     print()
@@ -26,8 +16,6 @@ if OpUser==1:
         print("Ahora que ha elegido el nombre, ingrese la contraseña a usar.")
         print("La contraseña puede tener letras, numeros y signos, pero debe serde un minimo de 8 caracteres y un maximo de 12")
         Con=(input(">> "))
-        Contraseñas.append(Con)
-        len(Con)
         if len(Con)>7 and len(Con)<13:
             while ConCon!=Con: 
                 print("Confirme su contraseña nuevamente:")
@@ -45,3 +33,19 @@ if OpUser==1:
             Con=input(">> ")
     else:
         print("Tu nombre de usuario no sigue con las indicaciones.")
+elif OpUser==2:
+    print("Usted ha seleccionado iniciar sesion en el programa de la 'tienda de cacería generica'.")
+    print()
+    print("Para confirmar su identidad, ingrese su nombre de usuario")
+    Nom=input(">> ")
+        if Nom in Usuarios==True:
+            print("Para confirmar su identidad como '",Nom,"' ingrese la contraseña de la cuenta.")
+            Con=input(">> ")
+            while Con in Contraseñas==False:
+                print("La contraseña ingresado no es correcta, intente nuevamente.")
+                Con=input(">> ")
+
+            if Con in Contraseñas==True:
+                    print("Bienvenido usuario '",Nom,"' espero que tenga un buen dia.")
+        else: #Seria bueno si hubiera un while para volver a las opciones de usuario.
+            print("Nombre de usuario no encontrado, si no tiene cuenta de usuario registrese.")
