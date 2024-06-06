44cfe86e1fce3a78f6283bec4490a5520add8ac7
JOTAROchl <167789565+JOTAROchl@users.noreply.github.com>
2024-06-06 11:14:54 -0400

    Update TDC.1.12.py

--- TDC.1.1.py
+++ TDC.1.1.py
@@ -1,6 +1,6 @@
-Usuarios=[]
-Contraseñas=[]
-ContraseñaCon=0
+Usuarios=["ju216749340","th212147895"]
+Contraseñas=["contragen1",""]#aqui pon tu contraseña
+ContraseñaCon=[]
 Inventario=[]
 OpUser=0
 Nom=0
@@ -17,12 +17,12 @@ OpUser=int(input(">> "))
 if OpUser==1:
     print("Usted ha seleccionado registrarse en el programa de la 'tienda de cacería generica'.")
     print()
-    print("Para registrarse, el nombre de usuario debe ser las primeras 2 letras del nombre y  el rut de este sin puntos o guiones, como el ejemplo a seguir;.")
-    print("th212147895")
+    print("Para registrarse, el nombre de usuario debe ser las primeras 2 letras del nombre y  el rut de este sin puntos o guiones, ej:")
+    print("'th212147895'.")
     Nom=(input(">> "))
     Usuarios.append(Nom)
     len(Nom)
-    if len(Nom)>9:
+    if len(Nom)==11:
         print("Ahora que ha elegido el nombre, ingrese la contraseña a usar.")
         print("La contraseña puede tener letras, numeros y signos, pero debe serde un minimo de 8 caracteres y un maximo de 12")
         Con=(input(">> "))
@@ -31,10 +31,17 @@ if OpUser==1:
         if len(Con)>7 and len(Con)<13:
             while ConCon!=Con: 
                 print("Confirme su contraseña nuevamente:")
-                ConCon=(input(">> "))
+                ConCon=input(">> ")
                 ContraseñaCon.append(ConCon)
                 if ConCon!=Con:
                     print("Reintente ingresar la contraseña")
+                    ContraseñaCon.pop()
+                    ConCon=input(">> ")
+                else:
+                    print("")
         else:
-            print("Tu contraseña no está en el rango establecido.")
-#Le voy a cambiar para que sea que al ingresar la contraseña equivocada sea devuelto a ingresar por primera vez la contraseña, pero esos son los cambios que hice
+            print("Tu contraseña no está en el rango establecido. intente nuevamente.")
+            Contraseñas.pop()
+            Con=input(">> ")
+    else:
+        print("Tu nombre de usuario no sigue con las indicaciones.")
