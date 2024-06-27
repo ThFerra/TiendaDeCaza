7ff305dee03afe325727c450ff9fc00cc9e66732
ThFerra <167789575+ThFerra@users.noreply.github.com>
2024-06-26 20:56:38 -0400

    Inventario casi listo, con datos añadidos

---
+++ TDC.py
@@ -0,0 +1,110 @@
+# main.py
+import libreria as lb
+OpUser=0
+Nom=""
+Con=0
+ConCon=0
+print("Hola bienvenidos a 'Tienda de caza generica'.")
+print("Inicie sesión para entrar (si no es así regístrese).")
+print()
+print("""--------CACERÍA GENERICA--------
+1.- Registrarse.
+2.- Iniciar sesión.
+--------------------------------""")
+OpUser=int(input(">> "))
+while OpUser!=3:
+    if OpUser==1:
+        OpUser=lb.registrar_usuario()
+    elif OpUser==2:
+        OpUser,Nom=lb.iniciar_sesion()
+print(f"Bienvenido usuario {Nom} a la 'tienda de cacería generica'.")
+print("Seleccione que acción desea hacer:")
+print(""" ----------MENU----------
+1.- Compra de artículos.
+2.- Inventario.
+3.- Áreas de caza (cercana o lejana).
+4.- Salir.
+------------------------""")
+Op1=int(input(">> "))
+while Op1!=4:
+    if Op1==1:  
+        print("Ha seleccionado 'Compra de artículos', a continuación están los siguientes artículos:")
+        print("""-------COMPRA DE ARTICULOS-------
+1.- Armas (arcos, pistolas, rifles, etc...) .
+2.- Munición (de las armas ya mencionadas) .
+3.- Trampas.
+---------------------------------""")
+        Op2=int(input(">> "))
+        if Op2==1:
+            print("Usted ha seleccionado Armas, a continuación están las armas disponibles:")
+            print("""-------COMPRA DE ARMAS-------
+1.- Arcos $100.000.
+2.- Pistolas $750.000.
+3.- Rifles $1.049.000.
+------------------------------""")
+            Op3=int(input(">> "))
+            if Op3==1:
+                print("Usted ha comprado un Arco de $100.000.")
+                print("Gracias por su compra.")
+            elif Op3==2:
+                print("Usted ha comprado una Pistola $750.000.")
+                print("Gracias por su compra.")
+            elif Op3==3:
+                print("Usted ha comprado un Rifle $1.049.000.")
+                print("Gracias por su compra.")
+        elif Op2==2:
+            print("Usted ha seleccionado Munición, a continuación están las armas disponibles:")
+            print("""--------MUNICION--------
+1.- Munición de Arcos [x1] $5.500.
+2.- Munición de Pistolas [x20] $12.000.
+3.- Munición de Rifles [x20] $20.000.
+------------------------""")
+            Op3=int(input(">> "))
+            if Op3==1:
+                print("Ha seleccionado 'Munición de Arcos [x1]', ingrese la cantidad de munición que desea comprar:")
+                CanMunArcos=int(input(">> "))
+                print(f"Usted ha seleccionado una cantidad de {CanMunArcos} Munición, con un total de ${lb.Cal_Ar(CanMunArcos)}.")
+            elif Op3==2:
+                print("Ha seleccionado 'Munición de Pistolas [x20]', ingrese la cantidad de munición que desea comprar:")
+                CanMunPistolas=int(input(">> "))
+                print(f"Usted ha seleccionado una cantidad de {CanMunPistolas} Munición, con un total de ${lb.Cal_Pi(CanMunPistolas)}.")
+            elif Op3==3:
+                print("Ha seleccionado 'Munición de Rifles [x20]', ingrese la cantidad de munición que desea comprar:")
+                CanMunRifles=int(input(">> "))
+                print(f"Usted ha seleccionado una cantidad de {CanMunRifles} Munición, con un total de ${lb.Cal_Ri(CanMunRifles)}.")
+
+        elif Op2==3:
+                print("Usted a seleccionado Trampas, a continuacion estan las Trampas disponibles:")
+                print("""--------TRAMPAS--------
+1.-Trampa animales aereos [No letal] $7.850.
+2.-Trampa animales terrestres [No letal] $9.990.
+-----------------------""")
+                Op3=int(input(">> "))
+                if Op3==1:
+                    print("A selecionado 'Trampa animales aereos [No letal]', ingrese la cantidad de trampas que desea comprar:")
+                    CanTAA=int(input(">>"))
+                    print(f"Usted ha comprado {CanTAA} de trampa/s de animales aereos, con un total de ${lb.Trampa_Ae(CanTAA)}.")
+                    
+                elif Op3==2:
+                    print("A selecionado 'Trampa animales terrestres [No letal]', ingrese la cantidad de trampas que desea comprar:")
+                    CanTAT=int(input(">>"))
+                    print(f"Usted ha comprado {CanTAT} de trampa/s de animales terrestres, con un total de ${lb.Trampa_Te(CanTAT)}.")
+    if Op1==2:
+        print("Ha seleccionado 'Inventario', a continuación seleccione que desea hacer:")
+        print("""-------INVENTARIO-------
+1.- Ver Inventario
+2.- Añadir Inventario
+-----------------------""")
+        Op2=int(input(">> "))
+        if Op2==1:
+            print("Armas:")
+            print("Arcos = ",{lb.InvArc})
+            print("Rifles = ",{lb.InvRif})
+            print("Pistolas = ",{lb.InvPis})
+            print("Municiones:")
+            print("Municion Arcos = ",{lb.InvMunArc})
+            print("Municion Rifles = ",{lb.InvMunRif})
+            print("Municion Pistolas = ",{lb.InvMunPis})
+            print("Trampas:")
+            print("Trampas animales Terrestres = ",{lb.InvTT})
+            print("Trampas animales Aereos = ",{lb.InvTA})
---
+++ libreria.py
@@ -0,0 +1,90 @@
+# libreria.py
+Usuarios=["jo216749340", "th212147895"]
+Contraseñas=["contragen1", "papasfritask"]
+ContraseñaCon=["contragen1", "papasfritask"]
+def registrar_usuario():
+    print("Usted ha seleccionado registrarse en el programa de la 'tienda de cacería generica'.")
+    print()
+    print("Para registrarse, el nombre de usuario debe ser las primeras 2 letras del nombre y el rut de este sin puntos o guiones, ej:")
+    print("'th212147895'.")
+    Nom=input(">> ")
+    Usuarios.append(Nom)
+    if len(Nom)==11:
+        print("Ahora que ha elegido el nombre, ingrese la contraseña a usar.")
+        print("La contraseña puede tener letras, números y signos, pero debe ser de un mínimo de 8 caracteres y un máximo de 12.")
+        Con=input(">> ")
+        Contraseñas.append(Con)
+        if 7<len(Con)<13:
+            ConCon=""
+            while ConCon!=Con: 
+                print("Confirme su contraseña nuevamente:")
+                ConCon=input(">> ")
+                if ConCon!=Con:
+                    print("ERROR: Las contraseñas no coinciden")
+                else:
+                    ContraseñaCon.append(ConCon)
+                    print("Bien, todo está correcto, ahora inicie sesión para entrar al programa.")
+                    print("Redireccionando a 'Iniciar sesión'...")
+                    return 2
+        else:
+            print("Tu contraseña no está en el rango establecido. Intente nuevamente.")
+            Contraseñas.pop()
+            return 1
+    else:
+        print("Tu nombre de usuario no sigue con las indicaciones.")
+        Usuarios.pop()
+        return 1
+
+def iniciar_sesion():
+    LogIn=0
+    while LogIn!=1:
+        print("Usted ha seleccionado iniciar sesión en el programa de la 'tienda de cacería generica'.")
+        print()
+        print("Para confirmar su identidad, ingrese su nombre de usuario")
+        Nom=input(">> ")
+        if Nom in Usuarios:
+            print(f"Para confirmar su identidad como '{Nom}', ingrese la contraseña de la cuenta.")
+            Con=input(">> ")
+            if Con in Contraseñas:
+                LogIn=1
+                return 3,Nom
+            else:
+                print("Contraseña incorrecta, si no tiene una regístrese.")
+                print("Redireccionando a 'Registrarse'...")
+                return 1,None
+        else: 
+            print("Nombre de usuario no encontrado, si no tiene cuenta de usuario regístrese.")
+            print("Redireccionando a 'Registrarse'...")
+            return 1,None
+        
+def Cal_Ar(cantidad):
+    return cantidad*5500
+def Cal_Pi(cantidad):
+    return cantidad*12000
+def Cal_Ri(cantidad):
+    return cantidad*20000
+
+def Trampa_Ae(CanTAA):
+    return CanTAA*7850
+def Trampa_Te(CanTAT):
+    return CanTAT*9990
+
+#Inventario
+#Contador para cada cosa, mensaje de error cuando compre mas de lo que hay, y entonces muestre lo disponible, inventario como lista que se divida entre las categorias de compra.
+#Verificar que la cantidad a comprar no sea mayor a la cantidad del inventario
+#Armas: Arcos, Rifles, Pistolas
+#Mun: MunArcos, MunRifles, MunPistolas
+#Trampas: Terrestres, Aereas
+#ARMAS
+InvArc=10
+InvRif=10
+InvPis=10
+#MUN
+InvMunArc=50
+InvMunRif=10
+InvMunPis=10
+#TRAMPAS
+InvTT=15
+InvTA=15
+
+Inventario=[InvArc,InvRif,InvPis,InvMunArc,InvMunRif,InvMunPis,InvTT,InvTA]
